29d59df7688444fde7f1e70c77963f4ebe353aa2
CodeSucker123 <eliasmorales2024s@gmail.com>
2024-12-27 20:35:52 -0600

    Update InstantShine_app.py

--- InstantShine_app.py
+++ InstantShine_app.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.title("Instant Shine")
 
+from PIL import image
+import cv2
+
+img = Image.open("Instant")
+#img = cv2.imread("Instant")
+st.image(
+  img , 
+  width = 800 , 
+  channels = "RGB"
+)
+
